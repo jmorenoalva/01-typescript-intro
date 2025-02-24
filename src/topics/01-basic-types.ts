e410e6e8fd85d321b948bfb14f55bcea8b87330c
Javier Moreno <jamorenoalva@gmail.com>
2025-02-24 17:41:00 -0500

    Seccion 3 - Bases de TypeScript - Clase 12. Tipos basicos y conceptos generales

---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,16 @@
+
+
+const name: string = "Strider";
+let hpPoints: number | string = 95;
+const isAlive: boolean = true;
+
+
+hpPoints='null';
+
+console.log(name);
+console.log(hpPoints);
+console.log(isAlive);
+
+console.log({name, hpPoints, isAlive});
+
+export {};
