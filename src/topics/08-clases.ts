57d0bf51190993fb1f250aa7389287253286ec69
Javier Moreno <jamorenoalva@gmail.com>
2025-02-25 12:29:45 -0500

    Clase 24. Extender una clase

--- src/topics/08-clases.ts
+++ src/topics/08-clases.ts
@@ -1,15 +1,21 @@
 
 
 export class Person{
-  public name: string;
-  private address: string;
+  // public name: string;
+  // private address: string;
 
-  constructor(){
-    this.name = 'Jose';
-    this.address='New York'
-  }
+  // constructor(name: string, address: string){
+  //   this.name = name;
+  //   this.address= address;
+  // }
+
+  constructor(
+    public name: string,
+    private address: string = 'No Address'
+  ){}
+
 }
 
-const ironman = new Person();
+const ironman = new Person('Tony', 'New York');
 
-console.log(ironman.address);
+console.log(ironman);
