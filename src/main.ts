74ea13304b71c684917c8ee8fc159df53df2f4af
Javier Moreno <jamorenoalva@gmail.com>
2025-02-25 15:03:52 -0500

    Clase 27. Decoradores

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,8 @@ import { setupCounter } from './counter.ts'
 // import './topics/06-function-destructuring.ts'
 // import './topics/07-import-export.ts'
 // import './topics/08-clases.ts'
-import './topics/09-generics.ts'
+// import './topics/09-generics.ts'
+import './topics/10-decorators.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `Hola mundo`
 
