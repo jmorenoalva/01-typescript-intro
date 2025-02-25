86e29c3545b8e09ebc131fc6602b07572afb4fe4
Javier Moreno <jamorenoalva@gmail.com>
2025-02-25 13:18:54 -0500

    Clase 26. Genericos

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ import { setupCounter } from './counter.ts'
 // import './topics/05-basic-destructuring.ts'
 // import './topics/06-function-destructuring.ts'
 // import './topics/07-import-export.ts'
-import './topics/08-clases.ts'
+// import './topics/08-clases.ts'
+import './topics/09-generics.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `Hola mundo`
 
