da11f767ba424cf6f236be33dbb661f902e800ad
Javier Moreno <jamorenoalva@gmail.com>
2025-02-25 12:53:36 -0500

    Clase 25. Priorizar composicion sobre herencia

--- src/topics/08-clases.ts
+++ src/topics/08-clases.ts
@@ -10,26 +10,44 @@ export class Person{
   // }
 
   constructor(
-    public name: string,
+    public firstName: string,
+    public lastName: string,
     private address: string = 'No Address'
   ){}
 
 }
 
 
-export class Hero extends Person {
+// export class Hero extends Person {
+
+//   constructor(
+//     public alterEgo: string,
+//     public age: number,
+//     public realName: string,
+//   ){
+//     super(realName, 'New York');
+//   }
+// }
+
+export class Hero {
+
+  // public person: Person;
 
   constructor(
     public alterEgo: string,
     public age: number,
     public realName: string,
-  ){
-    super(realName, 'New York');
+    public person: Person,
+  ) {
+
+    // this.person = new Person(realName);
+
   }
 }
 
 
 
-const ironman = new Hero('Ironman', 45, 'Tony');
+const tony = new Person("Tony", "Stark", "New York");
+const ironman = new Hero('Ironman', 45, 'Tony', tony);
 
 console.log(ironman);
