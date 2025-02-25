34a890ad5d762b9cfad83ee8146686047df4cde8
Javier Moreno <jamorenoalva@gmail.com>
2025-02-25 10:19:20 -0500

    Clase 18. Desestructuracion de arreglos

--- src/topics/05-basic-destructuring.ts
+++ src/topics/05-basic-destructuring.ts
@@ -23,12 +23,17 @@ const audioPlayer: AudioPlayer = {
 const song = 'New Song';
 
 const { song: anotherSong, songDuration: duration, details } = audioPlayer;
-// const {author: autor} = audioPlayer.details
+// const {author: autor} = audioPlayer.details //primero se desestructuro details para luego sacar el authot en otra constante
 const {author}=details;
 
-console.log("Song: ", song);
-console.log('Duration: ', duration);
-console.log('Author: ', author);
+// console.log("Song: ", song);
+// console.log('Duration: ', duration);
+// console.log('Author: ', author);
+
+const [,,trunk='Not found']: string[] = ['Goku', 'Vegeta'];
+// const trunk = dbz[3] || 'No hay personaje';
+
+console.error('Personaje 3: ', trunk);
 
 
 export{}
